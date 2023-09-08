5790200bf75e547575adb5246ff6748a7dffdff1
nezhenka <144461321+nezhenka@users.noreply.github.com>
2023-09-08 16:59:34 +0300

    Add files via upload

---
+++ db.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+    HOST: "dpg-cjtign5htt0c73e1edsg-a",
+    USER: "grady",
+    PORT: 5432,
+    PASSWORD: "wDPhed7oPcDT5Qvy2Wa0MkA1A8TiGHRa",
+    DB: "nezhenkadb",
+    dialect: "postgres",
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    }
+};
+//
+// module.exports = {
+//     HOST: "localhost",
+//     USER: "postgres",
+//     PORT: 5432,
+//     PASSWORD: "postgres",
+//     DB: "boomboom",
+//     dialect: "postgres",
+//     pool: {
+//         max: 5,
+//         min: 0,
+//         acquire: 30000,
+//         idle: 10000
+//     }
+// };
